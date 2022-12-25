76048dbf3300cc5bd609bb347a08ae7b6933fac8
Yosmar Hinestroza <yhinestroza@reuse.cl>
2022-12-25 04:36:54 -0300

    se agrega paginacion en principal

--- src/Services/Index.ts
+++ src/Services/Index.ts
@@ -8,5 +8,9 @@ export const webApiService = {
   getAllCharaters: async () => {
     const url = `${urlBase}character`
     return await axios(url, { method: 'GET', headers }).then((res: any) => res.data)
+  },
+  getAllPaginations: async (urlPage: string) => {
+    const url = urlPage
+    return await axios(url, { method: 'GET', headers }).then((res: any) => res.data)
   }
 }
