1cb3d6ad5a7269b542115654dc6bbc050620fc55
Yosmar Hinestroza <yosmarpc@gmail.com>
2022-12-27 20:43:07 -0300

    count footer

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { usePagesContext } from '../../context/PageContext'
 
 const Footer = () => {
+  const { data } = usePagesContext()
+  const _pages: number = data.informations ? Number(data.informations.next.split('=')[1]) : 0
+  console.log('split', data.informations ? data.informations.next.split('=')[1] : '')
   return (
     <section className='container-fluid'>
+      <div className='d-grid gap-2 d-md-flex justify-content-center '>
+        <h6 className='text-white'>Cantidad de Registro: {data.informations.count}</h6>
+        <h6 className='text-white'>Cantidad de Paginas: {_pages} de {data.informations.pages}</h6>
+      </div>
       <div className='text-center fs-6 py-3 text-white'>
         by Yosmar Hinestroza
       </div>
+
       <div className='d-flex justify-content-center py-3'>
         <a href='https://github.com/Yosmarpc' className='link-light ' target='_blank' rel='noreferrer'>github.com</a>
       </div>
