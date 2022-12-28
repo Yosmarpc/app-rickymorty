41eb003286370b9a7c8e0d1244a6a3971fb2ecef
Yosmar Hinestroza <yosmarpc@gmail.com>
2022-12-27 21:55:30 -0300

    se agrega numero de pagina en la paginaciones

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -3,13 +3,13 @@ import { usePagesContext } from '../../context/PageContext'
 
 const Footer = () => {
   const { data } = usePagesContext()
-  const _pages: number = data.informations ? Number(data.informations.next.split('=')[1]) : 0
-  console.log('split', data.informations ? data.informations.next.split('=')[1] : '')
+
+  const _pages: number = data.informations.length > 0 ? Number(data.informations.next.split('=')[1]) : 0
   return (
     <section className='container-fluid'>
       <div className='d-grid gap-2 d-md-flex justify-content-center '>
         <h6 className='text-white'>Cantidad de Registro: {data.informations.count}</h6>
-        <h6 className='text-white'>Cantidad de Paginas: {_pages} de {data.informations.pages}</h6>
+        <h6 className='text-white'>Cantidad de Paginas: {_pages === 0 ? 1 : 0} de {data.informations.pages}</h6>
       </div>
       <div className='text-center fs-6 py-3 text-white'>
         by Yosmar Hinestroza
--- src/Components/Paginations/Paginations.tsx
+++ src/Components/Paginations/Paginations.tsx
@@ -14,11 +14,14 @@ const Paginations = () => {
       setData({ ...data, loading: false, dataResult: response.results, informations: response.info })
     })
   }
-
+  // number page next
+  const _pagesNext: number = data.informations ? Number(data.informations.next.split('=')[1]) : 0
+  //  number page previous
+  const _pagesPrevious: number = data.informations.prev !== null ? Number(data.informations.prev.split('=')[1]) : 0
   return (
     <div className='d-grid gap-2 d-md-flex justify-content-md-center sticky-top'>
-      <Button className='btn btn-primary' onClick={() => getPaginations(data.informations.prev)} disabled={data.informations.prev === null}>Anterior</Button>
-      <Button className='btn btn-primary' onClick={() => getPaginations(data.informations.next)}>Siguiente</Button>
+      <Button className='btn btn-primary' onClick={() => getPaginations(data.informations.prev)} disabled={data.informations.prev === null}>Anterior {_pagesPrevious > 0 && _pagesPrevious}</Button>
+      <Button className='btn btn-primary' onClick={() => getPaginations(data.informations.next)}>Siguiente {_pagesNext}</Button>
     </div>
   )
 }
